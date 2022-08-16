efde166ee103a8aa824aa1bf38549a11754f09f1
Marcelo Almeida <2532492+marcelocra@users.noreply.github.com>
2022-08-16 12:58:21 +0000

    Release the blog to 'site' folder

--- release.sh
+++ release.sh
@@ -6,5 +6,6 @@ pnpm build
 # Tell github not to use jekyll, otherwise the site won't work.
 touch ./build/.nojekyll
 
-# Publish the 'build' folder to the gh-pages branch, without the 'build' prefix.
-git subtree push --prefix build origin gh-pages
+# I'm using 'site' folder in github as the deploy location.
+mv build site
+
