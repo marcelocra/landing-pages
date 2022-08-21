ab3994bb1e80e848f8ebee297991861ebd9f466f
Marcelo Almeida <2532492+marcelocra@users.noreply.github.com>
2022-08-21 22:52:45 +0000

    Add TailwindCSS to the project

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,6 @@ import { sveltekit } from '@sveltejs/kit/vite';
 /** @type {import('vite').UserConfig} */
 const config = {
 	plugins: [sveltekit()]
-	// server: {
-	// 	watch: {
-	// 		usePolling: true
-	// 	}
-	// }
 };
 
 export default config;
