44c6882b8c113df872a6111efd3e190ee55f3360
Marcelo Almeida <2532492+marcelocra@users.noreply.github.com>
2022-08-22 04:17:39 +0000

    Add docs to compress-images script

--- scripts/compress-images.js
+++ scripts/compress-images.js
@@ -6,7 +6,7 @@ import fs from 'node:fs/promises';
 
 const IMAGES = [
 	{
-		path: './src/lib/assets/profile.png',
+		oldImageFullPath: './src/lib/assets/profile.png',
 		newFilename: 'profile.min.webp',
 		overrideNewfile: false,
 		width: 192,
@@ -14,21 +14,57 @@ const IMAGES = [
 		srcs: ['./src/routes/+page.svelte']
 	}
 ];
-async function fixImports(src, imagePath, newFilename) {
-	const oldFilenameRE = new RegExp(path.basename(imagePath), 'g');
+
+/**
+ * Replaces the old image name with the new one in all source files.
+ *
+ * E.g.:
+ *
+ *  old:
+ *    import { image } from './src/profile.png';
+ *  new:
+ *    import { image } from './src/profile.min.webp';
+ *
+ * @param {string} src The path to the source file that should have its import fixed.
+ * @param {string} oldImagePath The path to the old image.
+ * @param {string} newFilename The name of the new image file.
+ */
+async function fixImports(src, oldImagePath, newFilename) {
+	// Create a regex to match the old file name. If the path is
+	// `./src/my/path/to/file.ext`, the regex with be `file.ext`.
+	const oldFilenameRE = new RegExp(path.basename(oldImagePath), 'g');
+
+	// Read the file content, replace the old filename with the new one and save
+	// the file.
 	const data = await fs.readFile(src, 'utf8');
 	const newContent = data.replace(oldFilenameRE, newFilename);
 	await fs.writeFile(src, newContent);
+
 	console.log(`Imports now have: '${newFilename}'`);
 }
 
-async function minifyImages(image) {
-	const format = path.extname(image.newFilename);
-	const imagePath = image.path;
-	const newImagePath = path.resolve(path.dirname(imagePath), image.newFilename);
+/**
+ * Minifies the input image and then replace its name in all related sources.
+ *
+ * @param {object} image An object containing all relevant information about
+ *  the image that should be minified.
+ * @returns Nothing. This function has side effects and save content to disk.
+ */
+async function minifyImage(image) {
+	// Get the desired file format from the new file extension. For example:
+	// ./src/profile.min.webp => webp
+	const format = path.extname(image.newFilename).slice(1);
 
+	const oldImagePath = image.oldImageFullPath;
+
+	// Uses the same path as the old image.
+	const newImagePath = path.resolve(path.dirname(oldImagePath), image.newFilename);
+
+	// Allow the user to override existing files in case they want. They will need
+	// to change the (currently) hardcoded array above, though.
 	if (!image.overrideNewfile) {
 		try {
+			// If this works, the file exists.
 			await fs.access(newImagePath);
 			console.log(`File '${image.newFilename}' already exists. Ignoring...`);
 			return;
@@ -38,16 +74,17 @@ async function minifyImages(image) {
 	}
 
 	try {
-		console.log(`Trying to convert: '${imagePath}'...`);
-		await sharp(imagePath)
+		console.log(`Trying to convert: '${oldImagePath}'...`);
+		await sharp(oldImagePath)
 			.resize({ width: image.width, height: image.height })
-			.toFormat(format.slice(1))
+			.toFormat(format)
 			.toFile(newImagePath);
 
 		const importsToFix = image.srcs.map(
-			async (src) => await fixImports(src, imagePath, image.newFilename)
+			async (src) => await fixImports(src, oldImagePath, image.newFilename)
 		);
 
+		// We want all imports converted before calling it a success.
 		await Promise.all(importsToFix);
 		console.log('Success!');
 	} catch (error) {
@@ -56,4 +93,4 @@ async function minifyImages(image) {
 	}
 }
 
-IMAGES.forEach((image) => minifyImages(image));
+IMAGES.forEach((image) => minifyImage(image));
